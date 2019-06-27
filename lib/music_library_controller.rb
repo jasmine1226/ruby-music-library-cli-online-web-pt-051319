d4046e1bde62cbb6c8c4322815f7591a61ba5a10
Jasmine Chen <jasmine1226@gmail.com>
2019-06-27 05:43:18 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -67,7 +67,7 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
-    input = gets.strip
+    input = gets.strip.to_i
     song = @last_list[input]
     puts "Playing #{song.name} by #{song.artist.name}" if input <= @last_list.length
   end
