080fdcac3c952b4112a14ed2684ab854ab4f99af
Jasmine Chen <jasmine1226@gmail.com>
2019-06-27 04:35:06 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,7 +24,7 @@ class MusicLibraryController
   end
 
   def list_songs
-    songs.each_with_index do |song, i|
+    Song.all.each_with_index do |song, i|
       puts "#{i+1}. #{song.artist} - #{song.name} - #{song.genre}"
     end
   end
